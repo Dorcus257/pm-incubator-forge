2dc0b53222b5cf02e2d12ef419fe894f8e2abe6a
ahmo2n <ahdvmf1379@gmail.com>
2025-12-22 16:42:40 +0900

    feat: [신채연] HTML, h1~ h6, p, ul, ol HTML 태그 학습 [PMIF-x]

--- chaeyeonshin/html5_css3/html-css-app/src/App.tsx
+++ chaeyeonshin/html5_css3/html-css-app/src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// 실행 방법 : npm run dev
+// 실행 이후 나타나는 local 주소에 접속
+
+// 현재 구성 특성 상 웹 브라우저에 바로 코드가 반영됨
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* h1태그는 제목을 표시할 때 사용합니다. */}
+        {/* 숫자 크기에 따라 사이즈가 변경됩니다. */}
+        <h1>First HTML5/CSS3</h1>
+        <h2>First HTML5/CSS3</h2>
+        <h3>First HTML5/CSS3</h3>
+        <h4>First HTML5/CSS3</h4>
+        <h5>First HTML5/CSS3</h5>
+        <h6>First HTML5/CSS3</h6>
+
+        {/* 주석을 다는 이유 */}
+        {/* 여러 사람들과 개발할 때 사람들과 생각 공유를 하기 위함 */}
+        <p>단락 paragragh 설정입니다.</p>
+
+        {/* 리스트(list) */}
+        {/* ul = unordered list */}
+          <ul>
+          <li>리스트 아이템 1</li>
+          <li>리스트 아이템 2</li>
+          <li>리스트 아이템 3</li>
+          <li>리스트 아이템 4</li>
+        </ul>
+
+        {/* ol = ordered list */}
+        <ol>
+          <li>리스트 아이템 1</li>
+          <li>리스트 아이템 2</li>
+          <li>리스트 아이템 3</li>
+          <li>리스트 아이템 4</li>
+        </ol>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
 export default App
+
